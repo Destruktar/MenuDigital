1b8fb4cfd7c4c36a09d96414ae0a9339b8f46183
Damian Arrieta <damian.el.grinch@gmail.com>
2026-03-19 22:01:18 -0300

    Cambio de precios

--- build/js/app.js
+++ build/js/app.js
@@ -6,7 +6,10 @@ const categorias = [
     new JsonFile('combos', '/build/json/combos.json'),
     new JsonFile('tostados-carlitos', '/build/json/tostados-carlitos.json'),
     new JsonFile('aperitivos', '/build/json/aperitivos.json'),
+    new JsonFile('empanadas', '/build/json/empanadas.json'),
     new JsonFile('menu', '/build/json/menu.json'),
+    new JsonFile('pastas', '/build/json/pastas.json'),
+    new JsonFile('guarniciones', '/build/json/guarniciones.json'),
     new JsonFile('sandwichs', '/build/json/sandwichs.json'),
     new JsonFile('tablas', '/build/json/tablas.json'),
     new JsonFile('cazuelas', '/build/json/cazuelas.json'),
@@ -15,10 +18,7 @@ const categorias = [
     new JsonFile('postres', '/build/json/postres.json'),
     new JsonFile('bebidas', '/build/json/bebidas.json'),
     new JsonFile('cervezas', '/build/json/cervezas.json'),
-    new JsonFile('licores-aperitivos', '/build/json/licores-aperitivos.json'),
-    new JsonFile('vinos', '/build/json/vinos.json'),
-    new JsonFile('cocteles', '/build/json/cocteles.json'),
-    new JsonFile('moktails', '/build/json/moktails.json')
+    new JsonFile('vinos', '/build/json/vinos.json')
 ]
 
 categorias.forEach(cat => cat.loadData());
